53e3f53fe56343d7863cc4b32d060e7015a0a36a
Brandon Shea <brandon.shea1994@icloud.com>
2025-02-13 15:15:58 -0330

    getters and setters

--- src/main/java/com/keyin/animalshelter/Animal.java
+++ src/main/java/com/keyin/animalshelter/Animal.java
@@ -1,4 +1,28 @@
 package com.keyin.animalshelter;
 
 public class Animal {
+
+    private String name;
+    private int arrivalTime;
+
+    public Animal(String name, int arrivalTime) {
+        this.name = name;
+        this.arrivalTime = arrivalTime;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int arrivalTime(){
+        return arrivalTime;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setArrivalTime(int arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
 }
