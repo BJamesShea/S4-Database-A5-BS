a565c637cca1ea9e59fda508ec3dd112ec5a42e0
Brandon Shea <brandon.shea1994@icloud.com>
2025-02-17 21:28:32 -0330

    constructors

--- src/main/java/com/keyin/animalshelter/AnimalShelter.java
+++ src/main/java/com/keyin/animalshelter/AnimalShelter.java
@@ -1,7 +1,17 @@
 package com.keyin.animalshelter;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class AnimalShelter {
-    public static void main(String[] args) {
+        private Queue<Dog> dogQueue;
+        private Queue<Cat> catQueue;
+        private int timeStamp;
+
+        public AnimalShelter() {
+            this.dogQueue = new LinkedList<>();
+            this.catQueue = new LinkedList<>();
+            this.timeStamp = 0;
 
     }
 }
